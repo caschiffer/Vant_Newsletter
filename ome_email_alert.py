9a0a0d79e82a0af3df275b1b8918a9c516ef8678
Cody Schiffer <cody.schiffer@sumitovant.com>
2020-07-28 23:40:01 -0700

    adding time sleep to send ome alerts to account for huge search term lists

--- ome_email_alert.py
+++ ome_email_alert.py
@@ -699,6 +699,7 @@ def send_ome_alerts_of_user(user):
     #sumitovant_list = internal_users + 
 
     user_alerts, user_alert_ids = get_documents.get_ome_alerts_of_user(user)
+    time.sleep(10)
     
     from_date = datetime.date.today() - datetime.timedelta(days=1)
     #from_date = datetime.date(2019,2,3)
@@ -882,16 +883,16 @@ def send_successful_search(user):
 #archits_alert_ids = send_ome_alerts_of_user('archit.sheth-shah')
 #emirs_alert_ids = send_ome_alerts_of_user('emir.haskovic')
 #anythonys_alert_ids = send_ome_alerts_of_user('anthony.bogachev')
-send_ome_alerts()
-time.sleep(120)
-codys_alert_completion = send_completion_notification('cody.schiffer')
-codys_search_completion = send_successful_search('cody.schiffer')
-time.sleep(120)
-yoanns_alert_completion = send_completion_notification('yoann.randriamihaja')
-time.sleep(120)
-aymans_search_completion = send_successful_search('ayman.mohammad')
-time.sleep(120)
-andrews_search_completion = send_successful_search('andrew.bogorad')
+# send_ome_alerts()
+# time.sleep(120)
+# codys_alert_completion = send_completion_notification('cody.schiffer')
+# codys_search_completion = send_successful_search('cody.schiffer')
+# time.sleep(120)
+# yoanns_alert_completion = send_completion_notification('yoann.randriamihaja')
+# time.sleep(120)
+# aymans_search_completion = send_successful_search('ayman.mohammad')
+# time.sleep(120)
+# andrews_search_completion = send_successful_search('andrew.bogorad')
 
 #print('done')
 #print('second debug')
@@ -910,7 +911,7 @@ andrews_search_completion = send_successful_search('andrew.bogorad')
 # email_address = 'cody.schiffer@sumitovant.com'
 # email_subject = 'DEBUGDEBUGDEBUG'
 
-# test_search_params, test_alert_title = get_documents.get_search_params_list('455')
+# test_search_params, test_alert_title = get_documents.get_search_params_list('462')
 # #print(test_search_params,'---- these are the test params')
 
 # ##test_url = construct_solr_search_url(test_search_terms, from_date=from_date)
